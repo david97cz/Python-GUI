858ecb6163903a3500e45c8fd7c9c0f6d118129c
david97cz <91188542+david97cz@users.noreply.github.com>
2024-02-06 15:20:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,63 @@
 import tkinter as tk
+from datetime import datetime
 
 
 class PanelBackground:
     def __init__(self,root):
         self.root = root
-        root.title("A simple GUI")
+        #tkinter variable
+        self.boolean_value1 = tk.BooleanVar()
+        root.title("Boiler SCADA")
         root.geometry("600x400")
-        root.configure(background="green")
+        root.configure(background="#7CB4B8")
 
+        #Create canvas
+        self.canvas = tk.Canvas(self.root, width=600, height=200, bg='white')
+        self.canvas.pack()
 
-        self.python_image= tk.PhotoImage(file="test_image.png")
-        self.label_image = tk.Label(root,image=self.python_image)
-        self.label_image.pack()
+        #Load image (only .gif)
+        self.python_image= tk.PhotoImage(file="boiler.gif")
 
-        self.label = tk.Label(root, text="",bg="blue",fg ="red",font=("Helvetica", 16))
+        #Put image into the canvas
+        self.canvas.create_image(0,0,anchor=tk.NW,image=self.python_image)
+
+        #Draw circle into the canvas
+        self.circle = self.canvas.create_oval(110, 110, 130, 130, outline="black", fill="blue", width=2)
+
+        #Create label
+        self.label = tk.Label(self.root, text="Boiler is OFF",bg="#57467B",fg ="#CAFE48",font=("Helvetica", 16))
         self.label.pack()
 
-        self.button = tk.Button(root, text="Button", command=self.print_text, bg="white",fg ="blue")
+        #Create Button
+        self.button = tk.Button(self.root, text="Print Status", command=self.print_text, bg="#57467B",fg ="#CAFE48")
         self.button.pack()
 
+        self.check_button = tk.Checkbutton(self.root,text='Boiler Switch',bg="#57467B",fg ="#CAFE48",selectcolor="#57467B",variable = self.boolean_value1, onvalue=True, offvalue=False, command=self.boolean_indicator)
+        self.check_button.pack()
+
     def print_text(self):
-        self.label.config(text="Hello World")
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        print("Current Time =", current_time)
+        if self.boolean_value1.get() == True:
+            print("Boiler is ON")
+        elif self.boolean_value1.get() == False:
+            print("Boiler is OFF")
+        print("__________________")
+
+
+    def boolean_indicator(self):
+        if self.boolean_value1.get() == True:
+            self.canvas.itemconfig(self.circle, fill='red')
+            self.label.config(text="Boiler is ON")
+        elif self.boolean_value1.get() == False:
+            self.canvas.itemconfig(self.circle, fill='blue')
+            self.label.config(text="Boiler is OFF")
 
 if __name__ == '__main__':
     root1 = tk.Tk()
     panel1 = PanelBackground(root1)
     root1.mainloop()
+
+
 
